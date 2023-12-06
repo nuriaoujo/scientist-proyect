7a0a30398689b8b44aedd64c85c1702194e3df7b
Nuria Oujo <nuriaoujo.n1@gmail.com>
2023-12-06 22:22:59 +0100

    homeProyect() function added in AppLogic

--- src/main/java/org/example/AppLogic.java
+++ src/main/java/org/example/AppLogic.java
@@ -4,13 +4,45 @@ import java.util.Scanner;
 
 public class AppLogic {
     private Proyects proyects;
-    private Scanner scanner = new Scanner(System.in);
+    private Scanner kb = new Scanner(System.in);
+    private int proyectOperation;
     public AppLogic() { }
 
+    public void homeProyect() {
+        System.out.println( "Bienvenido al gestor de proyectos científicos, \n" +
+                            "Selecciona una operación"
+        );
+        System.out.println( "(1). Crear un nuevo proyecto \n" +
+                            "(2). Mostrar información del proyecto"
+        );
+        do {
+            this.proyectOperation = kb.nextInt();
+            switch (this.proyectOperation) {
+                case 1:
+                    System.out.println("Has seleccionado: Crear un nuevo proyecto");
+                    createNewProyect();
+                    break;
+                case 2:
+                    System.out.println("Has seleccionado: Mostrar información del proyecto");
+                    showProyectDetails();
+                    break;
+                default:
+                    System.out.println("ERROR, el valor insertado no es correcto");
+
+                    break;
+            }
+        } while (this.proyectOperation < 1 || this.proyectOperation > 2);
+        kb.close();
+    }
+
     public void createNewProyect() {
-        System.out.println("Bienvenido al gestor de Proyectos, \n ¿Quieres generar un nuevo proyecto?");
+        System.out.println("Creando un nuevo proyecto...");
+        System.out.println("///////////////////////////////////////");
+        System.out.println( "Bienvenido al gestor de Proyectos, \n" +
+                            "¿Quieres generar un nuevo proyecto?"
+        );
         System.out.println("SI(1), NO(2)");
-        int createProyect = scanner.nextInt();
+        int createProyect = kb.nextInt();
 
         switch (createProyect) {
             case 1:
@@ -24,5 +56,10 @@ public class AppLogic {
                 createNewProyect();
                 break;
         }
+    }
+
+    public void showProyectDetails() {
+        System.out.println("Mostrando detalles del proyecto...");
+        System.out.println("///////////////////////////////////////");
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,11 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        AppLogic appLogic = new AppLogic();
+        appLogic.homeProyect();
     }
 }
