c37fd5a2d0bad7679e493b0a1168426271742a09
Nuria Oujo <nuriaoujo.n1@gmail.com>
2023-12-07 11:45:41 +0100

    AppLogic.java: homeProyect() with more conditions

--- src/main/java/org/example/AppLogic.java
+++ src/main/java/org/example/AppLogic.java
@@ -7,15 +7,18 @@ public class AppLogic {
     private int proyectOperation;
 
     private ManageProyects manageProyects;
+    private ManageScientist manageScientist;
 
     public AppLogic() { }
 
     public void homeProyect() {
-        System.out.println( "Bienvenido al gestor de proyectos científicos, \n" +
+        System.out.println( "Bienvenido al gestor de proyectos científicos. \n" +
                             "Selecciona una operación"
         );
         System.out.println( "(1). Crear un nuevo proyecto \n" +
-                            "(2). Mostrar información del proyecto"
+                            "(2). Mostrar información del proyecto \n" +
+                            "(3). Registrar un nuevo científico \n" +
+                            "(4). Mostrar los científicos registrados"
         );
 
         do {
@@ -31,19 +34,21 @@ public class AppLogic {
                     manageProyects.showProyectDetails();
                     closeProyectApp();
                     break;
+                case 3:
+                    System.out.println("Has seleccionado: Registrar un nuevo científico");
+                    manageScientist.registerNewScientist();
+                    closeProyectApp();
+                    break;
+                case 4:
+                    System.out.println("Has seleccionado: Mostrar los científicos registrados");
+                    manageScientist.showScientistDetails();
+                    closeProyectApp();
+                    break;
                 default:
                     System.out.println("ERROR, el valor insertado no es correcto");
                     break;
             }
-        } while (this.proyectOperation < 1 || this.proyectOperation > 2);
-    }
-
-
-    public void showScientistDetails() {
-        System.out.println("Mostrando los científicos...");
-        System.out.println("///////////////////////////////////////");
-
-        closeProyectApp();
+        } while (this.proyectOperation < 1 || this.proyectOperation > 4);
     }
 
     public void closeProyectApp() {
--- src/main/java/org/example/ManageScientist.java
+++ src/main/java/org/example/ManageScientist.java
@@ -1,4 +1,16 @@
 package org.example;
 
 public class ManageScientist {
+
+    public ManageScientist() { }
+
+
+    public void registerNewScientist() {
+
+    }
+
+    public void showScientistDetails() {
+        System.out.println("Mostrando los científicos...");
+        System.out.println("///////////////////////////////////////");
+    }
 }
