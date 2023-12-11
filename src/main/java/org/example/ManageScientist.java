784577b97a9ed0a89b0bb506b154b7ec5679b654
Nuria Oujo <nuriaoujo.n1@gmail.com>
2023-12-11 10:42:34 +0100

    ManageScientist.java: showScientistDetails() added

--- src/main/java/org/example/ManageScientist.java
+++ src/main/java/org/example/ManageScientist.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class ManageScientist {
@@ -11,9 +13,10 @@ public class ManageScientist {
     private Scanner kb = new Scanner(System.in);
     private Scanner kbTxt = new Scanner(System.in);
 
+    private List<Scientists> scientistsRegistrationList = new ArrayList<>();
+
     private Scientists scientists;
     public ManageScientist() { }
-
 
     public void registerNewScientist() {
         System.out.println("Registrando un nuevo científico...");
@@ -63,10 +66,43 @@ public class ManageScientist {
                 System.out.println("///////////////////////////////////////");
             }
         } while (proyectOperation != 1);
+
+        this.scientists = new Scientists(scientistName,scientistAge,scientistsDirection);
+        this.scientistsRegistrationList.add(scientists);
+        System.out.println("Registrando nuevo científico...");
+        System.out.println("El científico " + scientists.getName() + " ha sido registrado exitósamente");
+        System.out.println("///////////////////////////////////////");
     }
 
     public void showScientistDetails() {
         System.out.println("Mostrando los científicos...");
         System.out.println("///////////////////////////////////////");
+
+        if (scientistsRegistrationList.isEmpty()) {
+            System.out.println("ERROR: No hay ningún científico registrado");
+        } else {
+            System.out.println("Estos son los científicos registrados: ");
+            for(int i = 0; i < scientistsRegistrationList.size(); i++) {
+                System.out.println("(" + (i+1) + "). " + scientistsRegistrationList.get(i).getName());
+            }
+            System.out.println("Selecciona el científico del que deseas ver más detalles");
+
+            do{
+                this.proyectOperation = kb.nextInt();
+                System.out.println("Nombre: " + scientistsRegistrationList.get(proyectOperation -1).getName());
+                System.out.println("Edad: " + scientistsRegistrationList.get(proyectOperation -1).getAge());
+                System.out.println("Dirección: " + scientistsRegistrationList.get(proyectOperation -1).getAddress());
+            } while( proyectOperation < 1 || proyectOperation > (scientistsRegistrationList.size() + 1));
+
+            System.out.println("///////////////////////////////////////");
+            System.out.println("¿Quieres ver los detalles de otro científico registrado?");
+            System.out.println("SI(1), NO(2)");
+            this.proyectOperation = kb.nextInt();
+
+            if(proyectOperation == 1) {
+                System.out.println("...");
+                showScientistDetails();
+            }
+        }
     }
 }
